eba68d044ff21e2930e37cd82089f576b3cc9f5b
Alef Hamad <alefhamad@gmail.com>
2021-11-12 13:00:50 -0300

    ex01 done

---
+++ lista02/ex01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+    int main(){
+
+        int a;
+
+        printf("Type a whole numer: ")
+        scanf("%d", &a);
+
+        if(a < 0){
+            a = -a;
+        }
+        printf("Result: %d\n", a);
+    }
