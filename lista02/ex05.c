8c9d671aef2428fa133aabfa13c12deb18bebc24
Alef Hamad <alefhamad@gmail.com>
2021-11-12 14:06:58 -0300

    ex05 done

---
+++ lista02/ex05.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+    int main (){
+
+        int a, b;
+
+        printf("Type the fist number: ");
+        scanf("%d", &a);
+
+        printf("Type the second number: "); 
+        scanf("%d", &b);
+
+        if (b==0){
+            printf("INVALID ENTRY\n");
+        }
+        else{
+            printf("Quotient: %d, Remainder: %d\n", a/b, a%b);
+        }
+    }
