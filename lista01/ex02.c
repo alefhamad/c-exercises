a63d48892aaaabe603c83a68d446f40d5adf9052
Alef Hamad <alefhamad@gmail.com>
2021-11-12 09:29:24 -0300

    complete ex02

---
+++ lista01/ex02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+    int main(){
+
+        int  a, b;
+
+        printf("Digite um número: ");
+        scanf("%d", &a);
+
+        printf("Digite outro número: ");
+        scanf("%d", &b);
+
+        printf("O resultado da soma é: %d\n", a - b);
+    }
