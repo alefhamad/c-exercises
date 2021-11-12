f37d51771ddca9ab0f7042980c3545187b9fb72c
Alef Hamad <alefhamad@gmail.com>
2021-11-12 10:07:20 -0300

    ex05 done

---
+++ lista01/ex05.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+    int main(){
+
+        int a;
+
+        printf("Tupe in a number: ");
+        scanf("%d", &a);
+
+        printf("This number squared is: %d\n", a*a);
+        
+    }
