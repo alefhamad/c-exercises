8c980311a4e3936d56b4baff5739ae4019b98e17
Alef Hamad <alefhamad@gmail.com>
2021-11-12 09:38:48 -0300

    ex03 complete

---
+++ lista01/ex03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+    int main(){
+
+        int a, b;
+
+        printf("Type in a number: ");
+        scanf("%d", &a);
+
+        printf("Type in another number: ");
+        scanf("%d", &b);
+        
+        printf("The product of this two number is: %d\n", a*b);
+
+    }
