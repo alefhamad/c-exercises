131db3fbd4401a48a8e37c3c957a42ef7c0df2df
Alef Hamad <alefhamad@gmail.com>
2021-11-12 10:13:04 -0300

    ex06 done

---
+++ lista01/ex06.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main(){
+
+        int b, h, area, perimeter;
+
+        printf("Type in the base: ");
+        scanf("%d", &b);
+
+        printf("Type in the height: ");
+        scanf("%d", &h);
+
+        area = b * h;
+        perimeter = 2 * (b + h);
+
+        printf("The area is %d and the perimeter is %d", area, perimeter);
+    }
